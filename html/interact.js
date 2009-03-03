908f1261085091e956826143e667eaf2ddf54e47
Jayson Vantuyl <jvantuyl@engineyard.com>
2009-03-03 15:24:49 -0800

    Minor Refactor, Fixed Bug in Firefox

--- html/interact.js
+++ html/interact.js
@@ -11,13 +11,34 @@ function showHeader() {
   $('#toc').slideUp(swapDelay);
 };
 
+function highlightBox(el,event) {
+  var stale = $('.commentbox').filter('.tocentry')
+  var live = $('')
+  var targets = $(el).attr('target').split(',');
+  var count = 0;
+  var toc = $('#toc');
+  for (i=0;i<targets.length;i++) {
+    var target = $('#comment_' + targets[i]);
+    stale = stale.not(target);
+    live = live.add(target)
+    count += target.size();
+  };
+  stale.removeClass('tocentry')
+  live.addClass('tocentry')
+  stale.slideUp(swapDelay); 
+  live.slideDown(swapDelay);
+  showToc();
+  if (count) {
+    event.stopPropagation();
+  };
+};
+
 function showToc() {
   var toc = $('#toclist');
   var live = $('.commentbox').filter('.tocentry').children('.tochead')
   toc.empty();
   for (var i = 0; i < live.length; i++) {
     var t = $(live[i]).text()
-    console.log(t);
     toc.append('<li>' + t + '</li>');
   };
   $('#toc').slideDown(swapDelay);
@@ -75,25 +96,7 @@ $(function(){
 
   // Comment Dynamic Pop-Up Functionality
   $('.hotzone').click(function(event){
-    var stale = $('.commentbox').filter('.tocentry')
-    var live = $('')
-    var targets = $(this).attr('target').split(',');
-    var count = 0;
-    var toc = $('#toc');
-    for (i=0;i<targets.length;i++) {
-      var target = $('#comment_' + targets[i]);
-      stale = stale.not(target);
-      live = live.add(target)
-      count += target.size();
-    };
-    stale.removeClass('tocentry')
-    live.addClass('tocentry')
-    stale.slideUp(swapDelay); 
-    live.slideDown(swapDelay);
-    showToc();
-    if (count) {
-      event.stopPropagation();
-    };
+    highlightBox(this,event);
   });
 
   // Initial Setup
